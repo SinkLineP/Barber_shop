15f8c55b5dbcb1d5f2ff233f5ec22f94bb286aff
SinkLineP <popovartem505@gmail.com>
2019-10-26 22:05:46 +0300

    create page "About"

--- project_one/app.rb
+++ project_one/app.rb
@@ -6,3 +6,7 @@ require 'sinatra/reloader'
 get '/' do
 	erb "Hello! my github: <a href=\"https://github.com/SinkLineP/\">SinkLine_P</a>, and my channel: <a href=\"https://www.youtube.com/channel/UCV3V0MWW0d5xx6T4pxjZauQ?view_as=subscriber/\">My YouTube</a>."
 end
+
+get '/about' do
+	erb :about
+end
